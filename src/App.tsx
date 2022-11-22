fc624649c5bf7b4210434ce9766c51dcb3c2f26b
Artem Chernyshev <artem.octopus@mail.ru>
2022-11-22 14:49:54 +0300

    allc: state data

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {products} from "./data/products";
 function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {products.map((product) => <Product product={product}/>)}
+      {products.map((product) => <Product product={product} key={product.id}/>)}
     </div>
   );
 }
